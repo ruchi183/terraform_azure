74019d5c69e5361247dc7e10c1a4d7f7a2525d6c
rudub <35490403+rudub@users.noreply.github.com>
2019-07-03 00:23:39 +0530

    Delete DJ.ps1

--- DJ.ps1
+++
@@ -1,5 +0,0 @@
-$domain = "terraform.com"
-$password = "welcome@12345"| ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\ruchi
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-Add-Computer -DomainName $domain -Credential $credential
